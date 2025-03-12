a8286f088a0070a530e59f0d093b09cb4853bc81
MrKivs <kivululewis@gmail.com>
2025-03-12 08:30:41 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
     setInput("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/chat", {
+      const response = await fetch("https://frida-b8ep.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
